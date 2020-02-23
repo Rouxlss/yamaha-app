d25a822c5e43aba3120544bc941aecb08b51e074
Arnoldo <arnol.ortizjs@gmail.com>
2020-02-23 13:59:45 -0600

    version 3

--- sw.js
+++ sw.js
@@ -5,45 +5,46 @@ const staticAssets = [
     './styles.css',
     './index.js',
     './manifest.webmanifest',
-    './img/logo.png'
+    './img/logo.png',
+    './Avenir\ Next.ttc'
 ];
-
 
 self.addEventListener('install', async e => {
     const cache = await caches.open(cacheName);
     await cache.addAll(staticAssets);
     return self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
+  });
+  
+  self.addEventListener('activate', e => {
     self.clients.claim();
-})
-
-self.addEventListener('fetch', async e => {
+  });
+  
+  self.addEventListener('fetch', async e => {
     const req = e.request;
     const url = new URL(req.url);
-
-    if (url.origin == location.origin ) {
-        e.respondWith(cacheFirst(req));
+  
+    if (url.origin === location.origin) {
+      e.respondWith(cacheFirst(req));
     } else {
-        e.respondWith(networkAndCache(req));
+      e.respondWith(networkAndCache(req));
     }
-});
-
-async function cacheFirst(req) {
+  });
+  
+  async function cacheFirst(req) {
     const cache = await caches.open(cacheName);
     const cached = await cache.match(req);
     return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
+  }
+  
+  async function networkAndCache(req) {
     const cache = await caches.open(cacheName);
     try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
+      const fresh = await fetch(req);
+      await cache.put(req, fresh.clone());
+      return fresh;
     } catch (e) {
-        const cached = await cache.match(req);
-        return cached;
+      const cached = await cache.match(req);
+      return cached;
     }
-}
+  }
+  
