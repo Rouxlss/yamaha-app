4bb0589116636f5df821d98aa6c936f692143004
Arnoldo <arnol.ortizjs@gmail.com>
2020-07-21 16:39:57 -0600

    sexta version

---
+++ js/registro.js
@@ -0,0 +1,5 @@
+// var btnRegistro = document.getElementById("registrar");
+
+// btnRegistro.addEventListener ( 'click' , ()=> {
+//     console.log('diste un click');
+// });
---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+var btnRegister = document.getElementById("botonModal");
+var btnSesion = document.getElementById("botonModal2");
+
+var modalRegistro = document.getElementById("ventanaRegistro");
+var modalSesion = document.getElementById("ventanaSesion");
+
+var back1 = document.getElementById("back-register");
+var back2 = document.getElementById("back-sesion");
+
+
+var letrasTitulo = document.getElementById("title-login");
+
+
+function subir() {
+    modalRegistro.classList.add("animation-modal");
+    letrasTitulo.classList.add("animation-letter");
+}
+
+function bajar() {
+    modalRegistro.classList.remove("animation-modal");
+    letrasTitulo.classList.remove("animation-letter");
+}
+
+btnRegister.addEventListener('click', function() {
+    subir();
+});
+
+back1.addEventListener('click', function() {
+    bajar()
+});
+
+btnSesion.addEventListener('click', function() {
+    subir();
+});
+
+back2.addEventListener('click', function() {
+    bajar()
+});
+
